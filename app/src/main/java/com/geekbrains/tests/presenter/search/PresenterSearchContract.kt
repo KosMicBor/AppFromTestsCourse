0e520a6adc9a5d74109f4574efff4a8616974d0c
KosMicBor <gaijin-dk@mail.ru>
2022-07-10 17:22:17 +0500

    Lesson_3 homework is ready

--- app/src/main/java/com/geekbrains/tests/presenter/search/PresenterSearchContract.kt
+++ app/src/main/java/com/geekbrains/tests/presenter/search/PresenterSearchContract.kt
@@ -2,8 +2,7 @@ package com.geekbrains.tests.presenter.search
 
 import com.geekbrains.tests.presenter.PresenterContract
 
+
 internal interface PresenterSearchContract : PresenterContract {
     fun searchGitHub(searchQuery: String)
-    //onAttach
-    //onDetach
 }
